9024fc5179f894afc3c6186a9dbf5625c479a1d6
Melissa0o0 <158860782+Melissa0o0@users.noreply.github.com>
2025-02-15 20:07:11 +0100

    api integration

--- src/script.js
+++ src/script.js
@@ -1,13 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function generatePoem(write) {
+  console.log(write.data);
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: write.data.answer,
     autoStart: true,
-    delay: 1,
-    cursor: "",
+    cursor: null,
+    delay: 20,
   });
 }
-
 let poemFormElement = document.querySelector("#form");
 poemFormElement.addEventListener("submit", generatePoem);
+
+function aiPoem(generate) {
+  generate.preventDefault();
+  let apiKey = "937eb44cb68833ao5cdt10179cf40b38";
+  let context =
+    "Make sure to keep it short and sweet, use uncomplicated punchlines,no offensive content and provide the answer in french .";
+  let prompt = "Create a poem";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+
+  axios.get(apiUrl).then(generatePoem);
+}
+
+let buttonElement = document.querySelector("#button");
+buttonElement.addEventListener("click", aiPoem);
