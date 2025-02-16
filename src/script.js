d3c5378782782929264d5a2d0837e1a93afb5291
Melissa0o0 <158860782+Melissa0o0@users.noreply.github.com>
2025-02-16 23:46:58 +0100

    user interface

--- src/script.js
+++ src/script.js
@@ -12,13 +12,15 @@ poemFormElement.addEventListener("submit", generatePoem);
 
 function aiPoem(generate) {
   generate.preventDefault();
+  let wordInput = document.querySelector("#search-input");
   let apiKey = "937eb44cb68833ao5cdt10179cf40b38";
   let context =
-    "Make sure to keep it short and sweet, use uncomplicated punchlines,no offensive content and provide the answer in french. Use HTML format";
-  let prompt = "Create a poem";
+    "Make sure to keep it short and sweet, use uncomplicated punchlines,no offensive content and provide the answer in french. Use HTML format, for example <br> Hellooooo <br/> ";
+  let prompt = `Create a poem about ${wordInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${wordInput.value} </div>`;
 
   axios.get(apiUrl).then(generatePoem);
 }
